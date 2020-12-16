836a32405570d437efde8134a24ccf7ec237b4b8
Sarah Xie <sarahwxie@gmail.com>
2020-12-16 12:40:49 -0800

    login page

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def index():
     return render_template("home.html")
 
 
+# connects /hello path of server to render hello.html
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.run(port='3000', host='127.0.0.1')
