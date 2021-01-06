ee2a1522a8399e4032e9b4aaf2081ed24e9e7373
Sarah Xie <sarahwxie@gmail.com>
2021-01-06 12:43:01 -0800

    changed css from inline to out

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+
 @app.route('/login', methods=['POST','GET'])
 def login():
     if request.method == "POST":
@@ -17,17 +18,21 @@ def login():
     else:
         return render_template("login.html")
 
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr}</h1>"
+
 
 @app.route('/newuser/')
 def new_user():
     return render_template("signup.html")
 
+
 @app.route('/search/')
 def search():
     return render_template("search.html")
+
 
 if __name__ == "__main__":
     # runs the application on the repl development server
