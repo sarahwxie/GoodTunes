4ad64b59e250a77138931de974f249e0c2e93ca8
idaa355 <idaa355@users.noreply.github.com>
2021-01-15 22:06:10 -0800

    added songs.py to git

--- songs.py
+++ songs.py
@@ -4,5 +4,5 @@ songsfile = open('songs.csv')
 songsreader = csv.reader(songsfile)
 songsdata = list(songsreader)
 
-for i in range(len(songsdata)):
-    print(songsdata[i][1] + " - " + songsdata[i][2])
+#for i in range(len(songsdata)):
+    #print(songsdata[i][1] + " - " + songsdata[i][2])
